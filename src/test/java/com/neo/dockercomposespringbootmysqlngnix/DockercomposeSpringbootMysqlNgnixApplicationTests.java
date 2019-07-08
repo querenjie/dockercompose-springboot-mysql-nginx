e1e6fc9f1ad1897f03fb9bc850bc9212623dc122
querenjie <347794892@qq.com>
2019-07-08 16:19:52 +0800

    first time commit and push

---
+++ src/test/java/com/neo/dockercomposespringbootmysqlngnix/DockercomposeSpringbootMysqlNgnixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.neo.dockercomposespringbootmysqlngnix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockercomposeSpringbootMysqlNgnixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
