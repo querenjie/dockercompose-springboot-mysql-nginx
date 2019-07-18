bdc556e3f4986e55b09de09e141b4a52304ee94b
querenjie <347794892@qq.com>
2019-07-18 22:25:23 +0800

    aa

--- src/main/java/com/neo/controller/VisitorController.java
+++ src/main/java/com/neo/controller/VisitorController.java
@@ -45,6 +45,12 @@ public class VisitorController {
         if (ip == null || "".equals(ip.trim()) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
         }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
         if (ip == null || "".equals(ip.trim()) || "unknown".equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
         }
